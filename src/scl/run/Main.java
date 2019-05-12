abd1e04b73c61856f2bb674871595266c6245cc9
Mathis B <mbl.85@web.de>
2019-05-12 15:19:03 +0200

    Added Projectstructure

---
+++ src/scl/run/Main.java
@@ -0,0 +1,12 @@
+package scl.run;
+
+public class Main {
+	
+	String menu =""
+			+ "Students Chain List\n"
+			+ "L  - Empty the list\n"
+			+ "ZN - Sort by name and print list\n"
+			+ "ZM - Sort by matriculation number and print list\n"
+			+ "";
+	
+}
