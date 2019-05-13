89165ecfe8e7cd040f47d9b86d9d8959d1ab1896
Mathis B <mbl.85@web.de>
2019-05-13 15:00:30 +0200

    Finished menustring

--- src/scl/run/Main.java
+++ src/scl/run/Main.java
@@ -7,7 +7,15 @@ public class Main {
 			+ "L  - Empty the list\n"
 			+ "ZN - Sort by name and print list\n"
 			+ "ZM - Sort by matriculation number and print list\n"
-			+ "";
+			+ "SN - Search by name\n"
+            + "SM - Search by matriculation number \n"
+            + "N  - Insert student\n"
+            + "LN - Remove student by name\n"
+            + "LM - Remove student by matriculation number\n"
+            + "MN - Change name\n"
+            + "MM - Change matriculation number\n"
+            + "S  - Save data\n"
+            + "E  - End programm\n";
 	
 	public static void main(String[] args) {
 		new Main().run();
