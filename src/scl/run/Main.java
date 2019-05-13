9255d3a8f79caa01884f4048433492362954ef76
Mathis B <mbl.85@web.de>
2019-05-13 20:09:34 +0200

    Added menu switch

--- src/scl/run/Main.java
+++ src/scl/run/Main.java
@@ -26,8 +26,10 @@ public class Main {
 	private Student[] data;
 	private StudentReader sr;
 	private StudentWriter sw;
+	private boolean loop;
 	
 	public Main() {
+		loop = true;
 		br = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			sr = new StudentReader();
@@ -49,7 +51,7 @@ public class Main {
 	}
 	
 	public void run() {
-		while(true) {
+		while(loop) {
 			System.out.println(menu);
 			System.out.print(">>> ");
 			String input = null;
@@ -59,7 +61,72 @@ public class Main {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			execute(input);
 		}
 	}
 	
+	private void execute(String command) {
+		switch(command) {
+		case "L": empty(); break;
+		case "ZN": sortName(); break;
+		case "ZM": sortMN(); break;
+		case "SN": searchName(); break;
+		case "SM": searchMN(); break;
+		case "N": insert(); break;
+		case "LN": removeName(); break;
+		case "LM": removeMN(); break;
+		case "MN": changeName(); break;
+		case "MM": changeMN(); break;
+		case "S": save(); break;
+		case "E": exit(); break;
+		}
+	}
+	
+	private void empty() {
+		
+	}
+	
+	private void sortName() {
+		
+	}
+	
+	private void sortMN() {
+		
+	}
+	
+	private void searchName() {
+		
+	}
+	
+	private void searchMN() {
+		
+	}
+	
+	private void insert() {
+		
+	}
+	
+	private void removeName() {
+		
+	}
+	
+	private void removeMN() {
+		
+	}
+	
+	private void changeName() {
+		
+	}
+	
+	private void changeMN() {
+		
+	}
+	
+	private void save() {
+		
+	}
+	
+	private void exit() {
+		loop = false;
+	}
 }
